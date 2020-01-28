762c9d9998f7b6f45a05771dddfa14fae39ad969
Grant Craig <grantsidneycraig@gmail.com>
2020-01-28 10:19:52 -0600

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -10,6 +10,6 @@ module.exports = {
       require('postcss-svgo'),
       require('postcss-nested'),
       require('autoprefixer'),
-      // require('cssnano')
+      require('cssnano')
     ],
   };
