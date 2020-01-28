5b4b4b37a23baaa0655552657afde65b838c5c88
Grant Craig <grantsidneycraig@gmail.com>
2020-01-28 10:10:45 -0600

    in progress

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    plugins: [
+      require('postcss-import'),
+      require('postcss-url'),
+      require('postcss-preset-env')({
+        browsers: 'last 2 versions',
+        stage: 0,
+      }),
+      require('postcss-inline-svg'),
+      require('postcss-svgo'),
+      require('postcss-nested'),
+      require('autoprefixer'),
+      // require('cssnano')
+    ],
+  };
